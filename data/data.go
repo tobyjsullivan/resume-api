1e3bb631ad7c6c7432cbcdf9f5a818c6dba42017
Toby Sullivan <tobyjsullivan@gmail.com>
2017-09-16 13:56:58 -0700

    Add some job history

--- data/data.go
+++ data/data.go
@@ -1,27 +1,138 @@
 package data
 
-import "net/url"
+import (
+	"net/url"
+	"time"
+)
+
+const (
+	dateFmt = "2006-01-02"
+)
 
 type Person struct {
 	FirstName string
 	MiddleName string
 	LastName string
 	Website *url.URL
+	Jobs []*Job
+}
+
+type Company struct {
+	Name string
+}
+
+type Job struct {
+	Company *Company
+	Roles []*Role
+}
+
+type Role struct {
+	StartDate *time.Time
+	EndDate *time.Time
+	Title string
 }
 
 type Database struct {
 	Me *Person
 }
 
-func LoadDatabase() *Database {
-	website, _ := url.Parse("https://tobysullivan.net")
-
-	return &Database{
+var (
+	website, _ = url.Parse("https://tobysullivan.net")
+	database = &Database{
 		Me: &Person{
 			FirstName: "Toby",
 			MiddleName: "Jay",
 			LastName: "Sullivan",
 			Website: website,
+			Jobs: []*Job{
+				{
+					Company: &Company{Name: "Klue"},
+					Roles: []*Role{
+						&Role{
+							Title: "Software Developer",
+							StartDate: date("2017-04-24"),
+							EndDate: nil,
+						},
+					},
+				},
+				{
+					Company: &Company{Name: "Teespring"},
+					Roles: []*Role{
+						&Role{
+							Title: "Software Developer",
+							StartDate: date("2016-01-03"),
+							EndDate: date("2016-12-21"),
+						},
+					},
+				},
+				{
+					Company: &Company{Name: "Hootsuite"},
+					Roles: []*Role{
+						&Role{
+							Title: "Software Developer (Contract)",
+							StartDate: date("2013-01-01"),
+							EndDate: date("2013-03-31"),
+						},
+						&Role{
+							Title: "Software Developer",
+							StartDate: date("2013-11-04"),
+							EndDate: nil,
+						},
+						&Role{
+							Title: "Lead Software Developer",
+							StartDate: nil,
+							EndDate: date("2015-12-24"),
+						},
+					},
+				},
+				{
+					Company: &Company{Name: "Metalogix"},
+					Roles: []*Role{
+						&Role{
+							Title: "Software Developer Co-op",
+							StartDate: nil,
+							EndDate: nil,
+						},
+						&Role{
+							Title: "Software Developer",
+							StartDate: nil,
+							EndDate: nil,
+						},
+						&Role{
+							Title: "Lead Software Developer",
+							StartDate: nil,
+							EndDate: nil,
+						},
+					},
+				},
+				{
+					Company: &Company{Name: "ShipConstructor"},
+					Roles: []*Role{
+						&Role{
+							Title: "Quality Assurance Co-op",
+							StartDate: nil,
+							EndDate: nil,
+						},
+						&Role{
+							Title: "Software Developer Co-op",
+							StartDate: nil,
+							EndDate: nil,
+						},
+					},
+				},
+			},
 		},
 	}
+)
+
+func date(value string) *time.Time {
+	t, err := time.Parse(dateFmt, value)
+	if err != nil {
+		return nil
+	}
+	return &t
+}
+
+func LoadDatabase() *Database {
+	return database
 }
--- schema/schema.go
+++ schema/schema.go
@@ -14,6 +14,44 @@ func resolveMe(db *data.Database) graphql.FieldResolveFn {
 
 
 
+var companyType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Company",
+		Fields: graphql.Fields{
+			"name": &graphql.Field{
+				Type: graphql.String,
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					company, ok := p.Source.(*data.Company)
+					if !ok {
+						return nil, errors.New("Couldn't cast to Company")
+					}
+
+					return company.Name, nil
+				},
+			},
+		},
+	},
+)
+
+var jobType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "Job",
+		Fields: graphql.Fields{
+			"company": &graphql.Field{
+				Type: companyType,
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					job, ok := p.Source.(*data.Job)
+					if !ok {
+						return nil, errors.New("Couldn't cast to Job")
+					}
+
+					return job.Company, nil
+				},
+			},
+		},
+	},
+)
+
 var personType = graphql.NewObject(
 	graphql.ObjectConfig{
 		Name: "Person",
@@ -62,9 +100,21 @@ var personType = graphql.NewObject(
 					return person.Website.String(), nil
 				},
 			},
+			"jobs": &graphql.Field{
+				Type: graphql.NewList(jobType),
+				Resolve: func(p graphql.ResolveParams) (interface{}, error) {
+					person, ok := p.Source.(*data.Person)
+					if !ok {
+						return nil, errors.New("Couldn't cast to person")
+					}
+
+					return person.Jobs, nil
+				},
+			},
 		},
 	},
 )
+
 
 func queryType(db *data.Database) *graphql.Object {
 	return graphql.NewObject(
